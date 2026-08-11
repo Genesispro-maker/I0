2df5677c0982247b0decfa56e79caee10e05e308
Genesispro-maker <genesisprog894@gmail.com>
2026-08-11 04:26:16 -0700

    feat:added some webpack configurations in my next.config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,43 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, 
+  poweredByHeader: false, 
+
   images: {
     remotePatterns: [
       {
-       protocol: "https",
-       hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+        hostname: "://githubusercontent.com",
       },
-
       {
         protocol: "https",
-        hostname: "*.codesandbox.io"
+        hostname: "*.codesandbox.io",
       },
-  ],
-  }
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: ["@codesandbox/sandpack-react", "lucide-react", "zod"],
+  },
+
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      };
+    }
+
+    if (!dev) {
+      config.devtool = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
